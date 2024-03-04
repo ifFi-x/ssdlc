72cb6fa0bb9499be55c739aad497e5a676c47f0a
ifFi-x <arfatariqxiif@gmail.com>
2024-03-04 13:45:50 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+// Load environment variables
+import "dotenv/config";
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
